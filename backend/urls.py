0eaa8edab4e7a72b3c139ecd9019115e08e65bf0
rangyia <jess.graham.dev@gmail.com>
2021-02-08 23:41:52 -0500

    Configure the backend urls for react app

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,9 @@
-# Django 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from api.views import FrontendAppView
 
-# Views
 urlpatterns = [
-    path('', include('api.urls')),
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+    re_path(r'^', FrontendAppView.as_view())
 ]
