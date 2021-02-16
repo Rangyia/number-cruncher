3e8d501e446fd6a1b43f2cb22d98807a564b3424
rangyia <jess.graham.dev@gmail.com>
2021-02-15 23:27:07 -0500

    Add cross origin access

--- frontend/src/store/actions/auth.js
+++ frontend/src/store/actions/auth.js
@@ -1,6 +1,9 @@
 import * as actionTypes from "./actionTypes";
 import axios from "axios";
 
+axios.defaults.xsrfCookieName = 'csrftoken'
+axios.defaults.xsrfHeaderName = 'X-CSRFToken'
+
 export const authStart = () => {
   return {
     type: actionTypes.AUTH_START,
